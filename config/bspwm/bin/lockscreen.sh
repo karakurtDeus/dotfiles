c45d30848fd61bba4303dc24df50ddbd6635a074
Dmytro <karakurt.deus@gmail.com>
2026-09-21 16:59:49 +0300

    change lockscreen

--- config/bspwm/bin/lockscreen.sh
+++ config/bspwm/bin/lockscreen.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-bg=1a1b26ff
-fg=c0caf5ff
-ring=15161eff
-wrong=f7768eff
-date=c0caf5ff
+bg=000000ff
+fg=C5C8C6ff
+ring=F0C674ff
+date=F0C674ff
 verify=9ece6aff
+wrong=f7768eff
 
 TEMP_IMAGE=/tmp/i3lock.png
 
@@ -16,18 +16,22 @@ ffmpeg -y -loglevel quiet \
   "$TEMP_IMAGE"
 
 i3lock -n --force-clock -i "$TEMP_IMAGE" --fill -e --indicator \
-  --radius=30 --ring-width=60 --inside-color=$bg \
-  --ring-color=$ring --insidever-color=$verify --ringver-color=$verify \
-  --insidewrong-color=$wrong --ringwrong-color=$wrong --line-uses-inside \
-  --keyhl-color=$verify --separator-color=$verify --bshl-color=$verify \
-  --time-str="%H:%M" --time-size=140 --date-str="%a, %d %b" \
-  --date-size=45 --verif-text="Verifying Password..." --wrong-text="Wrong Password!" \
-  --noinput-text="" --greeter-text="Type the password to Unlock" --ind-pos="300:610" \
-  --time-font="JetBrainsMono NF:style=Bold" --date-font="JetBrainsMono NF" --verif-font="JetBrainsMono NF" \
-  --greeter-font="JetBrainsMono NF" --wrong-font="JetBrainsMono NF" --verif-size=23 \
-  --greeter-size=23 --wrong-size=23 --time-pos="300:390" \
-  --date-pos="300:450" --greeter-pos="300:780" --wrong-pos="300:820" \
-  --verif-pos="300:820" --date-color=$date --time-color=$date \
-  --greeter-color=$fg --wrong-color=$wrong --verif-color=$verify \
+  --radius=80 --ring-width=8 \
+  --inside-color=$bg --ring-color=$ring \
+  --insidever-color=$bg --ringver-color=$verify \
+  --insidewrong-color=$bg --ringwrong-color=$wrong \
+  --keyhl-color=$fg --separator-color=$bg --bshl-color=$wrong \
+  --time-str="%H:%M" --time-size=140 --date-str="%a, %d %b" --date-size=45 \
+  --verif-text="Verifying Password..." --wrong-text="Wrong Password!" \
+  --noinput-text="" --greeter-text="" \
+  --ind-pos="w/2:h/2+70" \
+  --time-pos="w/2:iy-180" --date-pos="tx:ty+60" \
+  --verif-pos="w/2:iy+120" --wrong-pos="w/2:iy+120" \
+  --time-align=0 --date-align=0 --verif-align=0 --wrong-align=0 \
+  --time-font="JetBrainsMono NF:style=Bold" --date-font="JetBrainsMono NF" \
+  --verif-font="JetBrainsMono NF" --wrong-font="JetBrainsMono NF" \
+  --verif-size=23 --wrong-size=23 \
+  --time-color=$date --date-color=$date \
+  --verif-color=$verify --wrong-color=$wrong \
   --pointer=default \
   --pass-media-keys --pass-volume-keys
